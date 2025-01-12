e4948918e198c7307e6323d4f31c9543295f414b
Stephanie A. <stephabdullah@gmail.com>
2025-01-12 17:46:35 -0600

    creating navigation bar

---
+++ pages/Navbar.js
@@ -0,0 +1,17 @@
+// components/Navbar.js
+
+export default function Navbar() {
+    return (
+      <nav>
+        <ul className="navbar">
+          <li><a href="/"><i>welcome to...</i></a></li>
+          <li><a href="/about">about- </a></li>
+          <li><a href="/portfolio">projects- </a></li>
+          <li><a href="/portfolio">art&design- </a></li>
+          <li><a href="/portfolio">games| games| games- </a></li>
+          <li><a href="/contact">let's connect- </a></li>
+        </ul>
+      </nav>
+    );
+  }
+  
